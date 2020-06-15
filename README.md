cfaf74c79ce9d7f8c02ffee9e138b10f35be932c
Ravi <ravisaini1990@gmail.com>
2020-06-15 12:45:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# TheMovies
+![license](https://img.shields.io/badge/license-MIT%20License-blue.svg) 
+[![API](https://img.shields.io/badge/API-16%2B-brightgreen.svg?style=flat)](https://android-arsenal.com/api?level=16)
+[![Build Status](https://travis-ci.org/skydoves/TheMovies.svg?branch=master)](https://travis-ci.org/skydoves/TheMovies) 
+<a href="https://github.com/skydoves"><img alt="License" src="https://img.shields.io/static/v1?label=GitHub&message=skydoves&color=C51162"/></a><br>
+
+
+A simple project for [The Movie DB](https://www.themoviedb.org) based on Kotlin MVVM clean architecture and material design & animations.<br>
+
+![gif0](https://user-images.githubusercontent.com/24237865/44523477-f6681180-a714-11e8-8f48-2dd3ca7cc8c0.gif) 
+![gif1](https://user-images.githubusercontent.com/24237865/44523478-f6681180-a714-11e8-9597-c885977fe136.gif)
+
+## How to build on your environment
+Add your API key in local.properties file.
+```xml
+tmdb_api_key=YOUR_API_KEY
+```
+
+## Development process
+Based on Test-driven development.<br>
+1. API Service -> API Service Unit Test with api response mock files
+2. DAO -> DAO Unit Test
+3. Repository -> Repository Unit Test
+4. ViewModel -> ViewModel Unit Test
+5. DI & Refactoring
+6. Implmentating UI & Layouts <br><br>
+![tdd](https://user-images.githubusercontent.com/24237865/44525064-b572fb80-a71a-11e8-9930-e77cde96561f.png)
+
+## Architecture
+Based on mvvm architecture and repository pattern.<br><br>
+![architecture](https://user-images.githubusercontent.com/24237865/44525736-e9e7b700-a71c-11e8-8045-42c4478dd67e.png)
+
+## Specs & Open-source libraries
+- Minimum SDK 16
+- 100% Kotlin based
+- MVVM Architecture
+- Architecture Components (Lifecycle, LiveData, ViewModel, Room Persistence)
+- DataBinding
+- Material Design & Animations
+- The Movie DB API
+- [Dagger2](https://github.com/google/dagger) for dependency injection
+- [Retrofit2 & Gson](https://github.com/square/retrofit) for constructing the REST API
+- [OkHttp3](https://github.com/square/okhttp) for implementing interceptor, logging and mocking web server
+- [Glide](https://github.com/bumptech/glide) for loading images
+- [BaseRecyclerViewAdapter](https://github.com/skydoves/BaseRecyclerViewAdapter) for implementing adapters and viewHolders
+- [WhatIf](https://github.com/skydoves/whatif) - checking nullable object and empty collections more fluently
+- [Mockito-kotlin](https://github.com/nhaarman/mockito-kotlin) for Junit mock test
+- [Timber](https://github.com/JakeWharton/timber) for logging
+- [Stetho](https://github.com/facebook/stetho) for debugging persistence data & network packets
+- Ripple animation, Shared element transition
+- Custom Views [ExpandableTextView](https://github.com/Manabu-GT/ExpandableTextView)
+
+## Posting
+[Medium - Android MVVM Architecture Components using The Movie Database API](https://medium.com/@skydoves/android-mvvm-architecture-components-using-the-movie-database-api-8fbab128d7)
+
+## Find this library useful? :heart:
+Support it by joining __[stargazers](https://github.com/skydoves/TheMovies/stargazers)__ for this repository. :star: <br>
+And __[follow](https://github.com/skydoves)__ me for my next creations! 🤩
+
+## Supports :coffee:
+If you feel like support me a coffee for my efforts, I would greatly appreciate it. <br>
+<a href="https://www.buymeacoffee.com/skydoves" target="_blank"><img src="https://skydoves.github.io/sponsor.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
+
+# License
+```xml
+The MIT License (MIT)
+
+Copyright (c) 2018 skydoves
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+```
---
+++ app/src/main/java/com/skydoves/ravi/binding/RecyclerViewBinding.kt
@@ -0,0 +1,134 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Designed and developed by 2018 skydoves (Jaewoong Eum)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.skydoves.ravi.binding
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.skydoves.baserecyclerviewadapter.BaseAdapter
+import com.skydoves.baserecyclerviewadapter.RecyclerViewPaginator
+import com.skydoves.ravi.extension.bindResource
+import com.skydoves.ravi.extension.visible
+import com.skydoves.ravi.models.Resource
+import com.skydoves.ravi.models.Review
+import com.skydoves.ravi.models.Status
+import com.skydoves.ravi.models.Video
+import com.skydoves.ravi.models.entity.Movie
+import com.skydoves.ravi.models.entity.Person
+import com.skydoves.ravi.models.entity.Tv
+import com.skydoves.ravi.view.adapter.MovieListAdapter
+import com.skydoves.ravi.view.adapter.PeopleAdapter
+import com.skydoves.ravi.view.adapter.ReviewListAdapter
+import com.skydoves.ravi.view.adapter.TvListAdapter
+import com.skydoves.ravi.view.adapter.VideoListAdapter
+import com.skydoves.ravi.view.ui.main.MainActivityViewModel
+import com.skydoves.whatif.whatIfNotNullOrEmpty
+
+@BindingAdapter("adapter")
+fun bindRecyclerViewAdapter(view: RecyclerView, adapter: BaseAdapter) {
+  view.adapter = adapter
+}
+
+@BindingAdapter("adapterMovieList")
+fun bindAdapterMovieList(view: RecyclerView, resource: Resource<List<Movie>>?) {
+  view.bindResource(resource) {
+    val adapter = view.adapter as? MovieListAdapter
+    adapter?.addMovieList(it)
+  }
+}
+
+@BindingAdapter("moviePagination")
+fun bindMoviePagination(view: RecyclerView, viewModel: MainActivityViewModel) {
+  RecyclerViewPaginator(
+    recyclerView = view,
+    isLoading = { viewModel.getTvListValues()?.status == Status.LOADING },
+    loadMore = { viewModel.postMoviePage(it) },
+    onLast = { false }
+  ).run {
+    currentPage = 1
+  }
+}
+
+@BindingAdapter("adapterPersonList")
+fun bindAdapterPersonList(view: RecyclerView, resource: Resource<List<Person>>?) {
+  view.bindResource(resource) {
+    val adapter = view.adapter as? PeopleAdapter
+    adapter?.addPeople(it)
+  }
+}
+
+@BindingAdapter("personPagination")
+fun bindPersonPagination(view: RecyclerView, viewModel: MainActivityViewModel) {
+  RecyclerViewPaginator(
+    recyclerView = view,
+    isLoading = { viewModel.getPeopleValues()?.status == Status.LOADING },
+    loadMore = { viewModel.postPeoplePage(it) },
+    onLast = { false }
+  ).run {
+    currentPage = 1
+  }
+}
+
+@BindingAdapter("adapterTvList")
+fun bindAdapterTvList(view: RecyclerView, resource: Resource<List<Tv>>?) {
+  view.bindResource(resource) {
+    val adapter = view.adapter as? TvListAdapter
+    adapter?.addTvList(it)
+  }
+}
+
+@BindingAdapter("tvPagination")
+fun bindTvPagination(view: RecyclerView, viewModel: MainActivityViewModel) {
+  RecyclerViewPaginator(
+    recyclerView = view,
+    isLoading = { viewModel.getTvListValues()?.status == Status.LOADING },
+    loadMore = { viewModel.postTvPage(it) },
+    onLast = { false }
+  ).run {
+    currentPage = 1
+  }
+}
+
+@BindingAdapter("adapterVideoList")
+fun bindAdapterVideoList(view: RecyclerView, resource: Resource<List<Video>>?) {
+  view.bindResource(resource) {
+    val adapter = view.adapter as? VideoListAdapter
+    adapter?.addVideoList(it)
+    it.data.whatIfNotNullOrEmpty {
+      view.visible()
+    }
+  }
+}
+
+@BindingAdapter("adapterReviewList")
+fun bindAdapterReviewList(view: RecyclerView, resource: Resource<List<Review>>?) {
+  view.bindResource(resource) {
+    val adapter = view.adapter as? ReviewListAdapter
+    adapter?.addReviewList(it)
+    it.data.whatIfNotNullOrEmpty {
+      view.visible()
+      view.setHasFixedSize(true)
+    }
+  }
+}
